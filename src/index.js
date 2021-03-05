ccbbbd8a81cf888b61823a0ee0c4ec1dce3d58db
Ryan Wholey <ryan@takescoop.com>
2021-03-04 17:58:19 -0800

    Handle parse json credentials error correctly

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ const Harbormaster = require('./lib/harbormaster')
 const deploy = require('./deploy')
 
 async function main () {
-  const {
-    client_email: clientEmail,
-    private_key: privateKey
-  } = JSON.parse(Buffer.from(process.env.SONAR_CREDENTIALS, 'base64').toString('utf-8'))
+
+  let clientEmail
+  let privateKey
+  try {
+    ({
+      client_email: clientEmail,
+      private_key: privateKey
+    } = JSON.parse(Buffer.from(process.env.SONAR_CREDENTIALS, 'base64').toString('utf-8')))
+  } catch (err) {
+    return core.setFailed(err)
+  }
 
   core.setSecret(privateKey)
 
