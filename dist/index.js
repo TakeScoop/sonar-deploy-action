88d7ddbe56e3a62e9211bfff9d2868598bf61b05
Ryan Wholey <ryan@takescoop.com>
2021-03-18 20:46:13 -0700

    Update dist

--- dist/index.js
+++ dist/index.js
@@ -54747,10 +54747,9 @@ async function deploy (harbormaster, config, context) {
 
     core.info('Successfully released')
   } catch (err) {
-    core.error(err)
-    core.error(err.stack)
-    core.error(err.message)
-    return core.setFailed(`${err.message}\n${err.stack}`)
+    console.error(err.response)
+
+    return core.setFailed(err)
   }
 }
 
